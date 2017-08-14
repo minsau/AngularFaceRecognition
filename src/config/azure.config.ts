a252be4ed57d46c36bba3bd7bbe8670b5c5f09c4
minsau <minsau2@gmail.com>
2017-08-14 13:23:54 -0500

    Primer commit con proyecto iniciado

---
+++ src/config/azure.config.ts
@@ -0,0 +1,5 @@
+export const AzureConfig = {
+  apiKey: '597a9ddbeeec4717a386bb543c108971',
+  apiKeySecond: '597a9ddbeeec4717a386bb543c108971',
+  endpoint: 'https://westus.api.cognitive.microsoft.com/face/v1.0' 
+}
